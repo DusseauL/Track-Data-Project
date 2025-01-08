43f7e655afdc1f40af62ca667d0afeccc95e4739
DusseauL <dusseaul55@gmail.com>
2025-01-08 17:55:45 -0500

    2nd commit: added more features and utilized a cursor

--- trackapp.py
+++ trackapp.py
@@ -6,40 +6,53 @@ Please choose an option:
 1) add a time into the database
 2) see all times in the database
 3) see best time for an event
-4) exit
+4) sort times based on an event
+5) see all times for an event
+6) see average time for an event
+7) exit
 
 your selection:
 """
 
-
-connection = trackdatabase.connect()
-
-trackdatabase.create_table(connection)
-
+trackdatabase.create_table()
 
 def menu():
     user_input = input(MENU)
-    while user_input != "4":
-
+    while user_input != "7":
         if user_input == "1":
 
             name = input("enter name:")
             event = input("enter event")
             time = input("enter time")
 
-            trackdatabase.add_time(connection,name,time,event)
+            trackdatabase.add_time(name,time,event)
 
-        if user_input == "2":
+        elif user_input == "2":
 
-            print(trackdatabase.get_times(connection))
+            print(trackdatabase.get_times())
 
-        if user_input == "3":
+        elif user_input == "3":
 
+            event = input("enter an event:")
+            print(trackdatabase.get_best_time(event))
+
+
+        elif user_input == "4":
+
+            event = input("enter an event:")
+            print(f"times for {event}:")
+            print(trackdatabase.get_sorted_times_by_event(event))
+
+        elif user_input == "5":
+            event = input("enter an event:")
+            print(f"times for {event}:")
+            print(trackdatabase.get_times_per_event(event))
+        elif user_input == "6":
             event = input("enter an event")
-            print(trackdatabase.get_best_time(connection, event))
-
-        if int(user_input) > 4:
+            print(f"average time of event")
+            print(trackdatabase.get_average_time(event))
+        else:
             print("please choose a number in the menu")
-            
+
         user_input = input(MENU)
 menu()
--- trackdatabase.py
+++ trackdatabase.py
@@ -1,35 +1,45 @@
 import sqlite3
 
-CREATE_TABLE = "CREATE TABLE IF NOT EXISTS times (id INTEGER PRIMARY KEY, name STRING, time FLOAT, event STRING);"
+
+
+CREATE_TABLE = "CREATE TABLE IF NOT EXISTS times (id PRIMARY KEY, name STRING, time FLOAT, event STRING);"
 ADD_TIMES = "INSERT INTO times (name, time, event) VALUES (?,?,?);"
-GET_TIMES = "SELECT * FROM times;"
+GET_TIMES = "SELECT name,time FROM times;"
 CLEAR_TIMES = "DELETE FROM times;"
-GET_BEST_TIME = "SELECT * FROM times WHERE event = ? ORDER BY time ASC LIMIT 1;"
-GET_WORST_TIME = "SELECT * FROM times WHERE event = ? ORDER BY DESC LIMIT 1;"
+GET_BEST_TIME = "SELECT name, time FROM times WHERE event = ? ORDER BY time ASC LIMIT 1;"
+GET_WORST_TIME = "SELECT name, time FROM times WHERE event = ? ORDER BY DESC LIMIT 1;"
+GET_SORTED_TIMES_EVENT = "SELECT name, time FROM times WHERE event = ? order by time ASC"
+GET_TIMES_EVENT = "SELECT name, time FROM times WHERE event = ?"
 
-def connect():
-    return sqlite3.connect("data.db")
+connection = sqlite3.connect("data.db")
+c = connection.cursor()
 
-def create_table(connection):
-    with connection:
-        connection.execute(CREATE_TABLE)
+def create_table():
+        c.execute(CREATE_TABLE)
     
-def add_time(connection,name, time, event):
-    with connection:
-        connection.execute(ADD_TIMES,(name,time,event))
+def add_time(name, time, event):
+        c.execute(ADD_TIMES,(name,time,event))
 
-def get_times(connection):
-    with connection:
-        return connection.execute(GET_TIMES).fetchall()
+def get_times():
+        return (c.execute(GET_TIMES).fetchall())
 
-def clear_database(connection):
-    with connection:
-        connection.execute(CLEAR_TIMES)
+def clear_database(c):
+        c.execute(CLEAR_TIMES)
 
-def get_best_time(connection, event):
-    with connection:
-        return connection.execute(GET_BEST_TIME, (event,)).fetchone()
+def get_best_time(event):
+        return c.execute(GET_BEST_TIME, (event,)).fetchone()
     
-def get_worst_time(connection, event):
-    with connection:
-        return connection.execute(GET_WORST_TIME, (event,)).fetchone()
+def get_worst_time(event):
+        return c.execute(GET_WORST_TIME, (event,)).fetchone()
+    
+def get_sorted_times_by_event(event):
+        return c.execute(GET_SORTED_TIMES_EVENT, (event,)).fetchall()
+    
+def get_times_per_event(event):
+        return (c.execute(GET_TIMES_EVENT, (event,)).fetchall())
+def get_average_time(event):
+        table = (c.execute(GET_TIMES_EVENT, (event,)).fetchall())
+        sum = 0
+        for row in table:
+                sum += row[1]
+        return sum/len(table)
